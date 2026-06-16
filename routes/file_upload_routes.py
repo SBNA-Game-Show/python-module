f974658dd9fc554be0a0eca6ca077cd588487eae
BhojGC <gcbhoj@gmail.com>
2026-06-15 22:12:31 -0400

    - added new route for upload_file - added to swagger - removed bearer

--- routes/file_upload_routes.py
+++ routes/file_upload_routes.py
@@ -6,6 +6,6 @@ from controller.file_upload_controller import process_uploaded_file
 
 upload_file_bp = Blueprint("upload_file_bp",__name__)
 
-
-upload_file_bp.route("/upload",methods=["POST"])(process_uploaded_file)
+upload_file_controller = swag_from("../swaggerdocs/file_uploads/upload_file.yml")(process_uploaded_file)
+upload_file_bp.route("/upload",methods=["POST"])(upload_file_controller)
 
